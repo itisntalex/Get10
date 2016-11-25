9b973823b407fac6b520dc9603a4ec9222541d96
iamcottonball <alexandrev.jamet@gmail.com>
2016-11-25 09:24:46 +0100

    Mechanims to manage button coloration

--- src/game/controllers/eventlisteners/BoutonEventListener.java
+++ src/game/controllers/eventlisteners/BoutonEventListener.java
@@ -1,5 +1,6 @@
 package game.controllers.eventlisteners;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,6 +21,35 @@ public class BoutonEventListener implements ActionListener {
 		int value = this.view.getModel().getButtonValue(source.getRow(), source.getColumn());
 		
 		this.view.getModel().incrementClickCounter();
+		
+		// All buttons now have there text written in black.
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				this.view.getBouton(i, j).setForeground(Color.BLACK);
+			}
+		}
+		
+		// Getting the source button position.
+		int row = source.getRow();
+		int column = source.getColumn();
+		
+		source.setForeground(Color.RED);
+		
+		if (this.view.getModel().getButtonValue((row - 1) % 5, column) == value) {
+			this.view.getBouton((row - 1) % 5, column).setForeground(Color.RED);
+		}
+		
+		if (this.view.getModel().getButtonValue((row + 1) % 5, column) == value) {
+			this.view.getBouton((row + 1) % 5, column).setForeground(Color.RED);
+		}
+		
+		if (this.view.getModel().getButtonValue(row, (column - 1) % 5) == value) {
+			this.view.getBouton(row, (column - 1) % 5).setForeground(Color.RED);
+		}
+		
+		if (this.view.getModel().getButtonValue(row, (column + 1) % 5) == value) {
+			this.view.getBouton(row, (column + 1) % 5).setForeground(Color.RED);
+		}
 		
 		System.out.println(String.format("Le boutton sur lequel vous avez appuyé avait pour valeur: %d", value));
 	}
